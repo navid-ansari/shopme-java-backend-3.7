78ba15f2777722044fc23861a79fc38ff5aa7f56
navid-ansari <navidansari25july@gmail.com>
2023-03-24 01:45:09 +0530

    disabled junit warnings mykong

--- src/test/java/com/example/shopmejavabackend/ShopmeJavaBackendApplicationTests.java
+++ src/test/java/com/example/shopmejavabackend/ShopmeJavaBackendApplicationTests.java
@@ -5,7 +5,6 @@ import com.example.shopmejavabackend.services.HealthCheckService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,7 +13,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@EnableConfigurationProperties
 @DisplayName("AppicationContext Test")
 class ShopmeJavaBackendApplicationTests {
 
